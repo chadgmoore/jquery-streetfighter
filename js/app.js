fce5876efbf2da80639af6abb584110513296fb4
chadgmoore <smap2d@gmail.com>
2015-01-21 15:47:12 -0500

    all systems go
    everything is functional.

--- js/app.js
+++ js/app.js
@@ -28,10 +28,22 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   })
 
+  $(document).keydown(function (e) {
+		if (e.keyCode == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-throwing').hide();
+    		$('.ryu-cool').show();
+			}
+	})
+
+
+
+
   });
 
 function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.25;
+  $('#hadouken-sound')[0].volume = 0.50;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
